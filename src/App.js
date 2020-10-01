c8fa10a6e4be364b250aaac50c36347540474b5e
KaranChauhan1907 <18bce040@nirmauni.ac.in>
2020-10-02 02:10:51 +0530

    API ADDED

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import Header from "./Components/Header"
 import InfoBox from "./Components/InfoBox"
 import Map from "./Components/Map"
 import './App.css';
+import { Card , CardContent } from '@material-ui/core';
 
 function App() {
+
+  const [countries,setCountries] = useState([])
+  const [country,setCountry] = useState("worldwide");
+  const [countryInfo,setCountryInfo] = useState({})
+
+  useEffect(()=>{
+    fetch("https://disease.sh/v3/covid-19/all")
+    .then((response)=>response.json())
+    .then((data)=>{
+    setCountryInfo(data)
+    })
+  })
+
+  useEffect(() => {
+      //async data
+      const getCountriesData = async() =>{
+          await fetch("https://disease.sh/v3/covid-19/countries")
+          .then((response)=>response.json())
+          .then((data)=>{
+              const countries = data.map((country)=>({
+                  name : country.country,
+                  value : country.countryInfo.iso2,
+                  }))
+              setCountries(countries)
+          })
+      }
+      getCountriesData()
+  }, [])
+
+  const ChangeCountry = async(e)=>{
+      const countrycode = e.target.value
+      const url =
+          countrycode === "worldwide"
+          ? "https://disease.sh/v3/covid-19/all"
+          :  `https://disease.sh/v3/covid-19/countries/${countrycode}`;
+
+          await fetch(url)
+          .then((response)=>response.json())
+          .then((data)=>{
+              setCountry(countrycode)
+              setCountryInfo(data);
+          })
+  }
+
+
   return (
     <div className="App">
-      <Header/>
-      <div className="app_stats">
-        <InfoBox title="Coronavirus Cases" cases={123} total={2000}/>
-        <InfoBox title="Recovered" cases={123} total={2000}/>
-        <InfoBox title="Deaths" cases={123} total={2000}/>
-      </div>
-      {/* Tables */}
-      {/* Graphs */}
-      <div className='app_map'>
-        <Map/>
-      </div>
+      <div className="app_left">
+        <Header onChange={ChangeCountry} country={country} countries={countries}/>
+        <div className="app_stats">
 
+          <InfoBox title="Coronavirus Cases" cases={countryInfo.todayCases} total={countryInfo.cases}/>
+          <InfoBox title="Recovered" cases={countryInfo.todayRecovered} total={countryInfo.recovered}/>
+          <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths}/>
+        </div>
+        <div className='app_map'>
+          <Map/>
+        </div>
+      </div>
+      <Card className="app_right">
+        <CardContent>
+          <h2>First</h2>
+        </CardContent>
+      </Card>
     </div>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,37 +1,17 @@
 import React , {useState , useEffect} from 'react'
 import { FormControl,Select,MenuItem } from '@material-ui/core';
-function Header() {
-
-    const [countries,setCountries] = useState([])
-    const [country,setCountry] = useState("worldwide");
-
-    useEffect(() => {
-        //async data
-        const getCountriesData = async() =>{
-            await fetch("https://disease.sh/v3/covid-19/countries")
-            .then((response)=>response.json())
-            .then((data)=>{
-                const countries = data.map((country)=>({
-                    name : country.country,
-                    value : country.countryInfo.iso2,
-                    }))
-                setCountries(countries)
-            })
-        }
-        getCountriesData()
-    }, [])
-
-    const ChangeCountry = (e)=>{
-        const countrycode = e.target.value
-        setCountry(countrycode)
-    }
+function Header({onChange,country,countries}) {
 
     return (
         <div className="header">
             <h1>COVID-19 TRACKER</h1>
             <FormControl>
                 
-            <Select variant="outlined" onChange={ChangeCountry} value={country}>
+            <Select 
+                variant="outlined" 
+                onChange={onChange} 
+                value={country}
+            >
                 <MenuItem value= "worldwide">WorldWide</MenuItem>
                 {countries.map((country) => (
                         <MenuItem value={country.value}>{country.name}</MenuItem>
